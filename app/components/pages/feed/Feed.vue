c16eba7a4e1bd9a7e6194eaf306362a8ac395675
Lance Meurs <lmeurs728@gmail.com>
2020-12-31 12:47:39 -0800

    Added v-for in feed

--- app/components/pages/feed/Feed.vue
+++ app/components/pages/feed/Feed.vue
@@ -1,9 +1,49 @@
 <template>
-	<Label text="Feed"></Label>
+	<StackLayout>
+		<LiftSummary v-for="liftSummary in liftSummaries" :key="liftSummary.summary"
+		:time="liftSummary.time"
+		:sets="liftSummary.sets"
+		:volume="liftSummary.volume"
+		:summary="liftSummary.summary" />
+	</StackLayout>
 </template>
 
 <script>
+import LiftSummary from "~/components/pages/feed/LiftSummary";
 export default {
+	components: {
+		LiftSummary: LiftSummary
+	},
+	data: function() {
+		return {
+			liftSummaries: [
+				{
+					time: "1:00",
+					sets: 4,
+					volume: 240,
+					summary: "Nice0"
+				},
+				{
+					time: "2:00",
+					sets: 5,
+					volume: 345,
+					summary: "Nice1"
+				},
+				{
+					time: "3:00",
+					sets: 6,
+					volume: 1233,
+					summary: "Nice2"
+				},
+				{
+					time: "4:00",
+					sets: 7,
+					volume: 979,
+					summary: "Nice3"
+				},
+			]
+		}
+	}
 };
 </script>
 
---
+++ app/components/pages/feed/LiftSummary.vue
@@ -0,0 +1,31 @@
+<template>
+	<StackLayout>
+		<Label text="Time:"></Label>
+		<Label :text="time"></Label>
+		<Label text="Sets:"></Label>
+		<Label :text="sets"></Label>
+		<Label text="Volume:"></Label>
+		<Label :text="volume"></Label>
+		<Label text="Summary:"></Label>
+		<Label :text="summary"></Label>
+	</StackLayout>
+</template>
+
+<script>
+import LiftSummary from "~/components/pages/feed/LiftSummary";
+export default {
+	props: {
+		time: String,
+		sets: Number,
+		volume: Number,
+		summary: String,
+	},
+	components: {
+		LiftSummary: LiftSummary
+	}
+};
+</script>
+
+<style scoped lang="scss">
+	
+</style>
