93a29c32dba9dc2c897bd0b9c2163d2f31486663
Lance Meurs <lmeurs728@gmail.com>
2021-01-06 13:39:35 -0800

    My first chart

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import VueDevtools from 'nativescript-vue-devtools'
 import Vue from "nativescript-vue";
+import RadChart from 'nativescript-ui-chart/vue';
 
 import Home from "./components/pages/home/Home";
 import MainPageRouter from "./components/mainPageRouter/MainPageRouter";
@@ -7,6 +8,7 @@ import StartWorkout from "./components/pages/lifts/presaved/StartWorkout";
 import EditWorkout from "./components/pages/lifts/customize/EditWorkout";
 
 Vue.use(VueDevtools);
+Vue.use(RadChart);
 
 Vue.config.silent = false;
 
--- app/components/pages/myProgress/IndividualLift.vue
+++ app/components/pages/myProgress/IndividualLift.vue
@@ -5,23 +5,42 @@
 			<Label :text="`${individualLiftHistory[0].title} History`" />
 		</ActionBar>
 		<StackLayout>
-			<Label text="Chart Here" />
+			<RadCartesianChart>
+				<CategoricalAxis v-tkCartesianHorizontalAxis />
+				<LinearAxis v-tkCartesianVerticalAxis />
+				<LineSeries v-tkCartesianSeries
+					:items="liftHistory"
+					categoryProperty="Country"
+					valueProperty="Amount" />
+			</RadCartesianChart>
 		</StackLayout>
 	</Page>
 	
 </template>
 
 <script>
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
+
 export default {
 	props: {
 		individualLiftHistory: Array
 	},
 	data: function(){
 		return {
-			
+			liftHistory: this.getLiftHistory()
 		}
 	},
-	
+	methods: {
+		getLiftHistory: function() {
+			return new ObservableArray([
+				{ Country: 'Germany', Amount: 15, SecondVal: 14, ThirdVal: 24, Impact: 0, Year: 0 },
+				{ Country: 'France', Amount: 13, SecondVal: 23, ThirdVal: 25, Impact: 0, Year: 0 },
+				{ Country: 'Bulgaria', Amount: 24, SecondVal: 17, ThirdVal: 23, Impact: 0, Year: 0 },
+				{ Country: 'Spain', Amount: 11, SecondVal: 19, ThirdVal: 24, Impact: 0, Year: 0 },
+				{ Country: 'USA', Amount: 18, SecondVal: 8, ThirdVal: 21, Impact: 0, Year: 0 }
+			]);
+		}
+	}
 };
 </script>
 
